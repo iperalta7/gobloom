e29ff381206d9fcdbc46efae8353494d26764a33
Isaiah Peralta <idp7@duck.com>
2025-04-12 18:00:34 -0400

    feat: use json to output

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 )
 
 type BloomFilter struct {
-	bits                    []bool
-	expectedSize            int // n
-	numberOfHashes          int // k
-	desiredFalseProbability float32
+	Bits                    []bool
+	ExpectedSize            int // n
+	HashCount               int // k
+	DesiredFalseProbability float32
 }
 
 func New(expectedSize int, desiredFalseProbability float32) (*BloomFilter, error) {
 	bf := &BloomFilter{
-		expectedSize:            expectedSize,
-		desiredFalseProbability: desiredFalseProbability,
+		ExpectedSize:            expectedSize,
+		DesiredFalseProbability: desiredFalseProbability,
 	}
 
 	bf.initBitsArray(float64(expectedSize), float64(desiredFalseProbability))
@@ -23,8 +24,12 @@ func New(expectedSize int, desiredFalseProbability float32) (*BloomFilter, error
 	return bf, nil
 }
 
-func (bf *BloomFilter) String() string {
-	return fmt.Sprintf("BloomFilter{expectedSize: %d, numberOfHashes: %d, falseProbability: %f}", bf.expectedSize, bf.numberOfHashes, bf.desiredFalseProbability)
+func (bf *BloomFilter) String() {
+	jsonBF, err := json.Marshal(bf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s", string(jsonBF))
 }
 
 // TODO
@@ -44,7 +49,7 @@ func (bf *BloomFilter) getSize(n float64, p float64) int {
 func (bf *BloomFilter) initBitsArray(n float64, p float64) {
 	m := bf.getSize(n, p)
 	array := make([]bool, m)
-	bf.bits = array
+	bf.Bits = array
 }
 
 // TODO
@@ -58,5 +63,5 @@ func main() {
 	if err != nil {
 		return
 	}
-	fmt.Printf("%s\n", bloomFilter.String())
+	bloomFilter.String()
 }
